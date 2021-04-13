f55f6105f8aa011f52ee34ccf3d399f747a378d5
Arlan Linux <marinhomoraisster@gmail.com>
2021-04-13 00:25:37 -0300

    recycler_view_1 - 03 - Testando performance do RecyclerView

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -1,6 +1,7 @@
 package br.com.alura.ceep.ui.recyclerview.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,15 +12,24 @@ import kotlinx.android.synthetic.main.item_nota.view.*
 
 class ListaNotasAdapter(private val context: Context, private val notas: List<Nota>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    companion object {
+        private var quantidadeViewCriada = 0
+        private var quantidadeBindView = 0
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        quantidadeViewCriada++
         val viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false)
+        Log.i("recyclerView adapter", "viewHolder criado: $quantidadeViewCriada")
         return NotaViewHolder(viewCriada)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        quantidadeBindView++
         val nota = notas[position]
         holder.itemView.item_nota_titulo.text = nota.titulo
         holder.itemView.item_nota_descricao.text = nota.descricao
+        Log.i("recyclerView adapter", "bind: posição $position quantidade $quantidadeBindView")
     }
 
     override fun getItemCount(): Int {
