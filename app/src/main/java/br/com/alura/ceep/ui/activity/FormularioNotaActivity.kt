c4e5a0234b94d71927e3fb4743536c47c9fa634a
Arlan Linux <marinhomoraisster@gmail.com>
2021-04-14 15:02:28 -0300

    recycler_view_1 - 04 - Adicionando o menu no formulário

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.kt
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.kt
@@ -2,11 +2,17 @@ package br.com.alura.ceep.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import br.com.alura.ceep.R
 
 class FormularioNotaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_nota)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_formulario_nota_salva, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
