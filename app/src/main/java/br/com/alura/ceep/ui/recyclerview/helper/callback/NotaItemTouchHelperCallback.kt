f0347cbfd8bf83f872d1e423553e7bfd90d6f950
Arlan Linux <marinhomoraisster@gmail.com>
2021-05-04 10:59:20 -0300

    recycler_view_2 - 04 - Adicionando comportamento de arrastar

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/helper/callback/NotaItemTouchHelperCallback.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/helper/callback/NotaItemTouchHelperCallback.kt
@@ -7,12 +7,15 @@ import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter
 
 class NotaItemTouchHelperCallback(private val adapter: ListaNotasAdapter) : ItemTouchHelper.Callback() {
 
+    private val dao: NotaDAO = NotaDAO()
+
     override fun getMovementFlags(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder
     ): Int {
         val marcacoesDeDeslize = ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
-        return makeMovementFlags(0, marcacoesDeDeslize)
+        val marcacoesDeArrastar = ItemTouchHelper.DOWN or ItemTouchHelper.UP or ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+        return makeMovementFlags(marcacoesDeArrastar, marcacoesDeDeslize)
     }
 
     override fun onMove(
@@ -20,12 +23,16 @@ class NotaItemTouchHelperCallback(private val adapter: ListaNotasAdapter) : Item
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder
     ): Boolean {
-        return false
+        val posicaoInicial = viewHolder.adapterPosition
+        val posicaoFinal = target.adapterPosition
+        dao.troca(posicaoInicial, posicaoFinal)
+        adapter.notifyDataSetChanged()
+        return true
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
         val posicaoNotaDeslizada = viewHolder.adapterPosition
-        NotaDAO().remove(posicaoNotaDeslizada)
+        dao.remove(posicaoNotaDeslizada)
         adapter.notifyDataSetChanged()
     }
 
